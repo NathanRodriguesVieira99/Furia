57f1dd1e3f5d07e58640197b099d40e816d41de5
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-23 16:45:01 -0300

    feat(react-query): install and setup react-query

---
+++ src/_contexts/providers/react-query-provider.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/_lib/react-query';
+export default function ReactQueryProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
