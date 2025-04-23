c8b2658abf5f32ba9f0543b846752821a34a3745
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-23 16:33:37 -0300

    feat(tests): install and setup jest & RTL

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: '@happy-dom/jest-environment',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
+};
+
+export default createJestConfig(config);
