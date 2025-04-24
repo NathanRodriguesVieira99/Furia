a596d0c8538426200154e428faab888b84a3490d
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-24 00:16:59 -0300

    feat(auth): implement authentication check on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+
 export default function Home() {
-  return <>hello world</>;
+  const router = useRouter();
+  const supabase = createClientComponentClient();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      router.replace(session ? '/auth/login' : '/protected');
+    };
+    checkAuth();
+  }, [router, supabase]);
+
+  return null;
 }
