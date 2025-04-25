e13ecbdff7617aba743de4a9d60ebfc189fe313c
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-25 13:34:57 -0300

    feat(heroText): create HeroText component

---
+++ src/components/layout/heroText.tsx
@@ -0,0 +1,22 @@
+export const HeroText = () => {
+  return (
+    <div className="text-md flex w-full flex-col items-center justify-center text-center font-semibold text-white sm:text-xl md:text-2xl lg:text-3xl">
+      <h2 className="mt-3 leading-8">
+        {' '}
+        O seu hub definitivo para acompanhar tudo sobre a{' '}
+        <span className="text-yellow-600">FURIA</span>{' '}
+      </h2>
+      <p className="mt-6 w-full p-2 leading-8.5">
+        Esports no cenário competitivo de CS2 em 2025! Desde emocionantes
+        clutches do KSCERATO até as estratégias ousadas do arT, nosso site é o
+        lugar onde os fãs se conectam, celebram e mergulham no universo da
+        <span className="ml-1 text-yellow-600">FURIA</span>.
+      </p>
+      <p className="mt-1 w-full p-2 leading-8.5">
+        {' '}
+        Aqui, você não só acompanha os melhores momentos do time, mas também
+        descobre mais sobre seus jogadores favoritos!.
+      </p>
+    </div>
+  );
+};
