a6058829a6ef7607242f5e44b466fac27e159065
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-25 13:36:21 -0300

    feat(headerLogo): create HeaderLogo component

---
+++ src/components/layout/headerLogo.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const HeaderLogo = () => {
+  return (
+    <div className="mr-24 flex w-1/2 items-center justify-center">
+      <Link href="/">
+        <Image
+          className="m-4 cursor-pointer"
+          src="/logo.svg"
+          alt="Logo Furia"
+          width={65}
+          height={65}
+        />
+      </Link>
+    </div>
+  );
+};
