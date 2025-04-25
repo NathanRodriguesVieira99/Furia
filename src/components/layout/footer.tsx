1228e6b3eb4b26df46d30af7d7cd324897bb5838
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-25 13:47:29 -0300

    feat(footer): create Footer component

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="text-md flex h-[500px] flex-col items-center text-center sm:text-xl md:text-2xl lg:text-3xl">
+      <h1 className="mt-6 leading-8">Nossos Parceiros</h1>
+    </footer>
+  );
+};
