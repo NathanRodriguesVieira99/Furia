6a5d1e7d4af65c0af21e02caeff229080985d4f8
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-28 18:18:04 -0300

    refactor(footer): delete footer component

--- src/app/protected/layout.tsx
+++ src/app/protected/layout.tsx
@@ -1,12 +1,10 @@
 import { Header } from '@/components/layout/header';
-import { Footer } from '@/components/layout/footer';
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
       <main className="">{children}</main>
-      <Footer />
     </>
   );
 };
