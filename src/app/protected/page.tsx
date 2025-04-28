80d722a64d50312ebb08b86c34d096b38b7cfe51
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-28 18:23:58 -0300

    feat(chatbot): add ChatBot component on page

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from '@/_lib/supabase/server';
 import { Hero } from '@/components/layout/hero';
 import { HeroText } from '@/components/layout/heroText';
 import { PlayersCard } from '@/components/layout/playersCard';
+import { ChatBot } from '@/components/ChatBot/ChatBot';
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -14,6 +15,9 @@ export default async function ProtectedPage() {
 
   return (
     <main className="container mx-auto">
+      <div className="fixed z-20 ml-[73%] flex items-start justify-start">
+        <ChatBot />
+      </div>
       <Hero />
       <HeroText />
       <PlayersCard />
