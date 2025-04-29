7ebd8298b8b0e9c4a42cb2b0a15743306eb5f2ee
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-29 20:55:54 -0300

    feat(types): add card component types

---
+++ src/_types/index.ts
@@ -0,0 +1,20 @@
+export interface CardImageProps {
+  src: string;
+  alt: string;
+}
+
+export interface CardDescriptionProps {
+  description: string;
+}
+
+export interface CardContentProps {
+  children: React.ReactNode;
+}
+
+export interface CardRootProps {
+  children: React.ReactNode;
+}
+
+export interface CardTitleProps {
+  title: string;
+}
