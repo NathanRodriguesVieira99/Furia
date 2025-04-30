bdf0b2f2e3e960488a91ed1a39cc2eaf1a3ad0a8
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-30 20:14:45 -0300

    feat(msw): setup MSW

---
+++ src/tests/mocks/handlers.ts
@@ -0,0 +1,8 @@
+// arquivo para colocar os mocks de chamadas a API
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+  http.get('', () => {
+    return HttpResponse.json({}, { status: 200 });
+  }),
+];
