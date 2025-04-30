5b3805f8b7af19e6bddc196b2fabd0f61251ec42
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-30 20:59:18 -0300

    feat(header): add animations to Header components

--- src/components/layout/headerLinks.tsx
+++ src/components/layout/headerLinks.tsx
@@ -1,17 +1,38 @@
+'use client';
+
+import { motion } from 'motion/react';
 import Link from 'next/link';
+
+const animation = {
+  initial: { opacity: 0, x: -100 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: -100 },
+};
 
 export const HeaderLinks = () => {
   return (
-    <div className="text-md ml-2 flex w-[100px] gap-4 pr-2 text-yellow-600 sm:text-lg lg:text-lg">
+    <motion.div className="text-md ml-2 flex w-[100px] gap-4 pr-2 text-yellow-600 sm:text-lg lg:text-lg">
       <Link
         href="#players_section"
         className="hover:text-yellow-500 hover:ease-in-out"
       >
-        <h1 className="font-semibold">Jogadores</h1>
+        <motion.h1
+          className="font-semibold"
+          transition={{ duration: 0.4, delay: 0.2 }}
+          {...animation}
+        >
+          Jogadores
+        </motion.h1>
       </Link>
       <Link href="#" className="hover:text-yellow-500 hover:ease-in-out">
-        <h1 className="font-semibold">Resultados</h1>
+        <motion.h1
+          className="font-semibold"
+          transition={{ duration: 0.5, delay: 0.3 }}
+          {...animation}
+        >
+          Resultados
+        </motion.h1>
       </Link>
-    </div>
+    </motion.div>
   );
 };
--- src/components/layout/headerLogo.tsx
+++ src/components/layout/headerLogo.tsx
@@ -1,18 +1,27 @@
+'use client';
+
+import { motion } from 'motion/react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export const HeaderLogo = () => {
   return (
-    <div className="mr-24 flex w-1/2 items-center justify-center">
+    <motion.div
+      className="mr-24 flex w-1/2 items-center justify-center"
+      initial={{ opacity: 0, y: -10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -10 }}
+      transition={{ duration: 0.5 }}
+    >
       <Link href="/">
         <Image
           className="m-4 cursor-pointer"
-          src="@/public/logo.svg"
+          src="logo.svg"
           alt="Logo Furia"
           width={65}
           height={65}
         />
       </Link>
-    </div>
+    </motion.div>
   );
 };
