df402571e766f6055f116c3fde09bb1298454c42
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-30 21:50:25 -0300

    fix(api): remove unused eslint comments

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 /*
 LÓGICA PRONTA FEITA COM AJUDA DE IA, POIS OS TUTORIAIS QUE SEGUI NÃO FUNCIONAVAM DE FORMA ALGUMA 
 */
