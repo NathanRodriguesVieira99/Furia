8fc1ba8c7d2e512507b07a65d53ecc8cfa3b8af4
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-30 21:37:23 -0300

    fix(jest): update tests folder import

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^@tests/(.*)$': '<rootDir>/src/tests/$1',
+    '^@tests/(.*)$': '<rootDir>/src/_tests/$1',
   },
 
   testEnvironmentOptions: {
